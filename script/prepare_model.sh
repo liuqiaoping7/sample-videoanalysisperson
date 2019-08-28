197d4c22621914fd4400affc312b35fb99f17c8b
ascendhuawei <44194787+ascendhuawei@users.noreply.github.com>
2019-08-28 23:52:49 +0800

    Update prepare_model.sh
    add C30 change

--- script/prepare_model.sh
+++ script/prepare_model.sh
@@ -38,20 +38,6 @@ tools_version=$1
 
 app_path="${script_path}/.."
 
-function download_prepare_model_script()
-{
-    echo "download download_model.sh..."
-    rm -rf ${script_path}/download_model.sh
-    
-    wget -O ${script_path}/download_model.sh "https://raw.githubusercontent.com/Ascend/models/master/download_model.sh" --no-check-certificate --quiet
-    if [ $? -ne 0 ];then
-        echo "ERROR: download failed, please check https://raw.githubusercontent.com/Ascend/models/master/download_modell.sh connetction."
-        rm -rf ${script_path}/download_model.sh
-        return 1
-    fi
-    return 0
-}
-
 function prepare()
 {
     model_names=$*
@@ -74,21 +60,6 @@ function prepare()
             fi
         done
     else
-        #for model_info in ${model_names}
-        #do
-            #model_name=`basename ${model_info}`
-            #if [ ! -f "${script_path}/${model_name}.om" ];then
-                #echo "ERROR: No ${model_name}.om in current path: ${script_path} for internet mode."
-                #return 1
-            #else
-                #mkdir -p ${app_path}/MyModel/${model_name}/device
-                #cp ${script_path}/${model_name}.om ${app_path}/MyModel/${model_name}/device/
-            #fi
-        #done
-        download_prepare_model_script
-        if [ $? -ne 0 ];then
-            return 1
-        fi
         bash ${script_path}/download_model.sh ${tools_version} ${model_names}
         
         if [ $? -ne 0 ];then
@@ -110,7 +81,8 @@ function prepare()
 
 main()
 {
-    model_names="computer_vision/object_detect/vgg_ssd computer_vision/classification/pedestrian computer_vision/classification/inception_age computer_vision/classification/inception_gender computer_vision/object_detect/face_detection"
+    #model_names="computer_vision/object_detect/vgg_ssd computer_vision/classification/pedestrian computer_vision/classification/inception_age computer_vision/classification/inception_gender computer_vision/object_detect/face_detection"
+    model_names="vgg_ssd pedestrian inception_age inception_gender face_detection"
     prepare ${model_names}
     if [ $? -ne 0 ];then
         exit 1
